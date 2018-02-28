a89821f5d7c67f3adb188d0468d788d91fc96fde
rufasa85 <rufasa85@gmail.com>
2018-02-27 22:37:09 -0800

    ajax

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log('i love cats!');
+$(document).ready(function() {
+	console.log('i love cats!');
+    $('#catPic').attr('src', 'http://www.thecatapi.com/api/images/get');
+	$.get('http://www.thecatapi.com/api/images/get').done(function(data){
+		console.log(data) ;
+	});
+});
